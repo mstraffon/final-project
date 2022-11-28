a02943a95a82d5c010493eb6bbe14d77f0e6986b
clairebakerr <115047973+clairebakerr@users.noreply.github.com>
2022-11-28 10:43:25 -0700

    Update cab_fp1.py

--- cab_fp1.py
+++ cab_fp1.py
@@ -14,7 +14,7 @@ df = df.astype({'Tracks':'int'})
 x = df['onehundred_million']
 y = df['Tracks']
 
-sns.regplot(x=df['onehundred_million'], y=df['Tracks'], scatter_kws={"color": "orchid"}, line_kws={"color": "darkmagenta"})
+sns.regplot(x=df['onehundred_million'], y=df['Tracks'], scatter_kws={"color": "mediumseagreen"}, line_kws={"color": "darkgreen"})
 plt.xlabel("Number of Tracks That Reached 100 Million", fontsize = 16)
 plt.ylabel("Number of Tracks Artist Has Released", fontsize = 14)
 plt.title("Number of 100 Million Records Dependent" +'\n'+ "on Number of Tracks",y=1,fontsize=16,fontweight='bold')
