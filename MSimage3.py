7ad1d04eefada34879b976f2809eb02f877e595a
mstraffon <113733835+mstraffon@users.noreply.github.com>
2022-12-04 09:38:48 -0700

    Update MSimage3.py

--- MSimage3.py
+++ MSimage3.py
@@ -1,21 +1,22 @@
 '''
 megan straffon
-image 2'''
+image 3'''
 #imports
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 #read csv
-df=pd.read_csv("spotify_top50_2021.csv",usecols=['energy','popularity'],nrows=50)
+df=pd.read_csv("spotify_top50_2021.csv",usecols=['energy','popularity'])
+
 
 #make graph
-plt.xlabel('Energy Level (0-1)',fontsize=20)
-plt.ylabel("Popularity Score (1-100)",fontsize=20)
+plt.xlabel('Popularity Score (1-100)',fontsize=20)
+plt.ylabel("Energy Level (0-1)",fontsize=20)
 plt.title("Are Popular Songs More Energetic?",fontsize=20)
 
-plt.scatter(df.energy,df.popularity,color="mediumseagreen",edgecolor="black")
-z=np.polyfit(df.energy, df.popularity,0)
+plt.scatter(df.popularity,df.energy,color="mediumseagreen",edgecolor="black")
+z=np.polyfit(df.popularity, df.energy,0)
 p=np.poly1d(z)
-plt.plot(df.energy, p(df.popularity),color='black')
+plt.plot(df.popularity, p(df.energy),color='black')
 plt.show()
